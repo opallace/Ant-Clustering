8802820b87f6fdc925a95b7cbe890a9a17fd5e16
WM <wallace0x7d0@protonmail.com>
2022-05-03 15:37:33 -0300

    Add files via upload

---
+++ ants.h
@@ -0,0 +1,49 @@
+#define NOT_CARRING 0;
+#define CARRING 1
+
+typedef struct Ant Ant;
+typedef struct Map Map;
+
+struct Ant{
+	int state;
+
+	Ant *ant;
+
+	int x, y;
+};
+
+struct Map{
+	int width;
+	int height;
+
+	int n_alive_ants;
+	int n_dead_ants;
+
+	Ant *alive_ants;
+	Ant *dead_ants;
+
+	Ant **alive_ants_grid;
+	Ant **dead_ants_grid;
+};
+
+
+void set_dead_ants_grid(Map *map, Ant *ant, int x, int y);
+void rem_dead_ants_grid(Map *map, int x, int y);
+Ant* get_dead_ants_grid(Map *map, int x, int y);
+
+void set_alive_ants_grid(Map *map, Ant *ant, int x, int y);
+void rem_alive_ants_grid(Map *map, int x, int y);
+Ant* get_alive_ants_grid(Map *map, int x, int y);
+
+int tem_formiga_morta(Map *map, int x, int y);
+int tem_formiga_viva(Map *map, int x, int y);
+int is_valid_coordinates(Map *map, int x, int y);
+int counts_dead_neighbor_ants(Map *map, Ant ant, int radius);
+
+Map* map_init();
+Ant* ant_init();
+void create_ants(Map *map);
+void move_ant(Map *map, Ant *ant);
+void map_iterate(Map* map);
+int verify_end(Map *map);
+void map_last_iterate(Map *map);
